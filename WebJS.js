5df5250be232ba31df28785b780c05c60c83bd46
codenation <codenation@codenations-MacBook-Pro.local>
2019-08-29 17:07:33 +0100

    final

---
+++ WebJS.js
@@ -0,0 +1,77 @@
+
+// function myFunction() {
+//   var x = document.getElementById("myDIV");
+//   if (x.style.display == "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+// }
+// function myFunction() {
+//   var text = document.getElementById("myDIV");
+//   if (text.style.display === "none") {
+//     text.style.display = "block";
+//   } else {
+//     text.style.display = "none";
+//   }
+// }
+
+
+
+function myFunction() {
+  x = document.getElementById("myDIV");
+  if (x.style.display === "none"){
+    x.style.display = "block"
+  }else {
+      x.style.display = "none"
+    }
+  }
+  function myFunction1() {
+    x = document.getElementById("text");
+    if (x.style.display === "none"){
+      x.style.display = "block"
+    }else {
+        x.style.display = "none"
+      }
+    }
+    function myFunction2() {
+      x = document.getElementById("text2");
+      if (x.style.display === "none"){
+        x.style.display = "block"
+      }else {
+          x.style.display = "none"
+        }
+      }
+      function myFunction3() {
+        x = document.getElementById("text3");
+        if (x.style.display === "none"){
+          x.style.display = "block"
+        }else {
+            x.style.display = "none"
+          }
+        }
+        function myFunction4() {
+          x = document.getElementById("text4");
+          if (x.style.display === "none"){
+            x.style.display = "block"
+          }else {
+              x.style.display = "none"
+            }
+          }
+          function myFunction5() {
+            x = document.getElementById("text5");
+            if (x.style.display === "none"){
+              x.style.display = "block"
+            }else {
+                x.style.display = "none"
+              }
+            }
+            function myFunction6() {
+              x = document.getElementById("text6");
+              if (x.style.display === "none"){
+                x.style.display = "block"
+              }else {
+                  x.style.display = "none"
+                }
+              }
